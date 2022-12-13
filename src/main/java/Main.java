6d0e1244305e1e90b4230819afd2db87555215b3
Daniyar <mdaniyar1995@gmail.com>
2022-12-13 21:34:57 +0300

    27

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,13 @@ import comparator.UniversityComparator;
 import enums.StudentComparatorType;
 import enums.UniversityComparatorType;
 import io.XlsReader;
+import io.XlsWriter;
+import model.Statistics;
 import model.Student;
 import model.University;
 import util.ComparatorUtil;
+import util.JsonUtil;
+import util.StatisticsUtil;
 
 import java.io.IOException;
 import java.util.List;
@@ -18,16 +22,36 @@ public class Main {
                 XlsReader.readXlsUniversities("src/main/resources/universityInfo.xlsx");
         UniversityComparator universityComparator =
                 ComparatorUtil.getUniversityComparator(UniversityComparatorType.YEAR);
-        universities.stream()
-                .sorted(universityComparator)
-                .forEach(System.out::println);
+        universities.sort(universityComparator);
+        String universitiesJson = JsonUtil.universityListToJson(universities);
+        System.out.println(universitiesJson);
+        List<University> universitiesFromJson = JsonUtil.jsonToUniversityList(universitiesJson);
+        System.out.println(universities.size() == universitiesFromJson.size());
+        universities.forEach(university -> {
+            String universityJson = JsonUtil.universityToJson(university);
+            System.out.println(universityJson);
+            University universityFromJson = JsonUtil.jsonToUniversity(universityJson);
+            System.out.println(universityFromJson);
+        });
 
-        List<Student> students =
-                XlsReader.readXlsStudents("src/main/resources/universityInfo.xlsx");
-        StudentComparator studentComparator =
-                ComparatorUtil.getStudentComparator(StudentComparatorType.AVG_EXAM_SCORE);
-        students.stream()
-                .sorted(studentComparator)
-                .forEach(System.out::println);
-    }
+            List<Student> students =
+                    XlsReader.readXlsStudents("src/main/resources/universityInfo.xlsx");
+            StudentComparator studentComparator =
+                    ComparatorUtil.getStudentComparator(StudentComparatorType.AVG_EXAM_SCORE);
+            students.sort(studentComparator);
+            String studentsJson = JsonUtil.studentListToJson(students);
+            System.out.println(studentsJson);
+            List<Student> studentsFromJson = JsonUtil.jsonToStudentList(studentsJson);
+            System.out.println(students.size() == studentsFromJson.size());
+            students.forEach(student -> {
+                String studentJson = JsonUtil.studentToJson(student);
+                System.out.println(studentJson);
+                Student studentFromJson = JsonUtil.jsonToStudent(studentJson);
+                System.out.println(studentFromJson);
+            });
+
+        List<Statistics> statisticsList = StatisticsUtil.createStatistics(students, universities);
+        XlsWriter.writeXlsStatistics(statisticsList, "statistics.xlsx");
+
+        }
 }
